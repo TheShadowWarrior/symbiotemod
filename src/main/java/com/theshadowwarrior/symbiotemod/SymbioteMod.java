3498a8cf1c5df48631f662709536489f66ee6538
TheShadowWarrior <90646255+TheShadowWarrior@users.noreply.github.com>
2025-06-09 14:23:20 +0300

    Update SymbioteMod.java

--- src/main/java/com/theshadowwarrior/symbiotemod/SymbioteMod.java
+++ src/main/java/com/theshadowwarrior/symbiotemod/SymbioteMod.java
@@ -1,1 +1,25 @@
+package com.theshadowwarrior.symbiotemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(SymbioteMod.MODID)
+public class SymbioteMod {
+    public static final String MODID = "symbiotemod";
+
+    public SymbioteMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Init logic here
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        // Client-specific stuff
+    }
+}
 
